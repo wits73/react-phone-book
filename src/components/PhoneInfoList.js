e5409800b99be7e434dd75fcd5f9371c405f6d04
Tae Kim <tkim25@kennesaw.edu>
2018-08-31 16:56:55 -0400

    CRUD filtered

--- src/components/PhoneInfoList.js
+++ src/components/PhoneInfoList.js
@@ -3,22 +3,35 @@ import React, { Component } from 'react';
 import PhoneInfo from './PhoneInfo';
 
 class PhoneInfoList extends Component {
-  static defaultProps = {
-    data: []
-  }
+    static defaultProps = {
+        data: [],
+        onRemove: () => console.warn('onRemove not defined'),
+        onUpdate: () => console.warn('onUpdate not defined'),
+    }
 
-  render() {
-    const { data } = this.props;
-    const list = data.map(
-      info => (<PhoneInfo key={info.id} info={info}/>)
-    );
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.data !== this.props.data;
+    }
 
-    return (
-      <div>
-        {list}    
-      </div>
-    );
-  }
+    render() {
+        console.log('render PhoneInfoList');
+        const { data, onRemove, onUpdate } = this.props;
+
+        const list = data.map(
+            info => (<PhoneInfo
+                key={info.id}
+                info={info}
+                onRemove={onRemove}
+                onUpdate={onUpdate}
+            />)
+        );
+
+        return (
+            <div>
+                {list}
+            </div>
+        );
+    }
 }
 
 export default PhoneInfoList;
