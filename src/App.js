04becc8866a45e0f311c1389917e89fb47193ddc
Abraham <wits73@hotmail.com>
2018-09-15 14:54:31 -0400

    added search

--- src/App.js
+++ src/App.js
@@ -16,10 +16,15 @@ class App extends Component {
         name: '홍길동',
         phone: '010-0000-0001'
       }
-    ]
+    ],
+    keyword:''
   }
 
-
+  handleChange = (e) => {
+    this.setState({
+      keyword: e.target.value,
+    });
+  }
   
   handleCreate = (data) => {
     const { information } = this.state;
@@ -47,16 +52,27 @@ class App extends Component {
   }
 
   render() {
-    const { information } = this.state;
+    const { information, keyword } = this.state;
+    const filteredList = information.filter(
+      info => info.name.indexOf(keyword) !== -1
+    );
     return (
       <div>
         <PhoneForm
           onCreate={this.handleCreate}
         />
 
+        <p>
+          <input 
+            placeholder="검색 할 이름을 입력하세요.." 
+            onChange={this.handleChange}
+            value={keyword}
+          />
+        </p>
+        <hr />
 
         <PhoneInfoList 
-          data={this.state.information}
+          data={filteredList}
           onRemove={this.handleRemove}  
           onUpdate={this.handleUpdate}
         />
