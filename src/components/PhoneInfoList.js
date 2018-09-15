65b78a1aa7019477f0dff9bd6fed54b6f892f89a
Abraham <wits73@hotmail.com>
2018-09-15 12:03:01 -0400

    basic array

--- src/components/PhoneInfoList.js
+++ src/components/PhoneInfoList.js
@@ -1,36 +1,16 @@
-// src/components/PhoneInfoList.js
-import React, { Component } from 'react';
-import PhoneInfo from './PhoneInfo';
+import React, { Component } from "react";
+import PhoneInfo from "./PhoneInfo";
 
 class PhoneInfoList extends Component {
     static defaultProps = {
-        data: [],
-        onRemove: () => console.warn('onRemove not defined'),
-        onUpdate: () => console.warn('onUpdate not defined'),
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-        return nextProps.data !== this.props.data;
-    }
+        data: []
+    };
 
     render() {
-        console.log('render PhoneInfoList');
-        const { data, onRemove, onUpdate } = this.props;
+        const { data } = this.props;
+        const list = data.map((info, index) => <PhoneInfo key={index} info={info} />);
 
-        const list = data.map(
-            info => (<PhoneInfo
-                key={info.id}
-                info={info}
-                onRemove={onRemove}
-                onUpdate={onUpdate}
-            />)
-        );
-
-        return (
-            <div>
-                {list}
-            </div>
-        );
+        return <div>{list}</div>;
     }
 }
 
